4b0d0e14c62cbc222f5640b9860331db3774ce50
Maxuel <lubiomaxuel@gmail.com>
2019-02-15 22:57:28 -0200

    Creates routes to projects

--- server/controllers/projectsController.js
+++ server/controllers/projectsController.js
@@ -2,10 +2,12 @@ const uuid = require('uuid');
 const db = require('../db/db');
 
 module.exports = {
+  // List All
   async index(req, res) {
     return res.json(db.get('projects').value());
   },
 
+  // Create
   async store(req, res) {
     const proj = req.body;
     proj.id = uuid();
@@ -15,5 +17,36 @@ module.exports = {
       .write();
 
     return res.json(proj);
+  },
+
+  // Find One
+  async findOne(req, res) {
+    res.json(
+      db
+        .get('projects')
+        .find({ id: req.body })
+        .value(),
+    );
+  },
+
+  // Update
+  async update(req, res) {
+    const proj = req.body;
+
+    db.get('projects')
+      .find({ id: proj.id })
+      .assign(proj)
+      .write();
+
+    res.json(proj);
+  },
+
+  // Delete
+  async delete(req, res) {
+    db.get('projects')
+      .remove({ id: req.body })
+      .write();
+
+    return res.json(req.body);
   },
 };
--- server/routes.js
+++ server/routes.js
@@ -4,7 +4,20 @@ const routes = express.Router();
 
 const projectsController = require('./controllers/projectsController');
 
+// List All
 routes.get('/api/projects', projectsController.index);
+
+// Create
 routes.post('/api/projects', projectsController.store);
+
+// Get by id
+app.get('/api/projects/:id', projectsController.findOne);
+
+// Update
+app.put('/api/projects/:id', projectsController.update);
+
+// Delete
+app.delete('/api/projects/:id', projectsController.delete);
+}
 
 module.exports = routes;
