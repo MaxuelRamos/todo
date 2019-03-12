361928b61718ac1b9856944dbcb79d80ba84e928
Maxuel <lubiomaxuel@gmail.com>
2019-03-12 01:17:04 -0300

    remove o backend java

--- backend/src/main/resources/import-dev.sql
+++
@@ -1,7 +0,0 @@
---
--- DADOS PARA TESTES
---
-
--- users
-
-INSERT INTO "user" (id, name, role, email, password) VALUES (nextval('public.hibernate_sequence'), 'PONTO Suporte', 'SUPPORT', 'suporte@ponto.com', '$2a$10$8SPbnpaQ1j8Zn.BkOUYrve5OJlipsOLKzx80kPNiDpG4yNsR/gA.C');
